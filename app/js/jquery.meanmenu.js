5981c6dfea82c3b72dcd89b254eb331584390335
Shumskih <alex.shumskih@gmail.com>
2017-05-12 11:46:26 +0300

    Added mobile menu

--- app/js/jquery.meanmenu.js
+++ app/js/jquery.meanmenu.js
@@ -31,12 +31,12 @@
 						meanMenuTarget: jQuery(this), // Target the current HTML markup you wish to replace
 						meanMenuContainer: 'body', // Choose where meanmenu will be placed within the HTML
 						meanMenuClose: "X", // single character you want to represent the close menu button
-						meanMenuCloseSize: "18px", // set font size of close button
+						meanMenuCloseSize: "22px", // set font size of close button
 						meanMenuOpen: "<span /><span /><span />", // text/markup you want when menu is closed
 						meanRevealPosition: "right", // left right or center positions
 						meanRevealPositionDistance: "0", // Tweak the position of the menu
 						meanRevealColour: "", // override CSS colours for the reveal background
-						meanScreenWidth: "480", // set the screen width you want meanmenu to kick in at
+						meanScreenWidth: "991", // set the screen width you want meanmenu to kick in at
 						meanNavPush: "", // set a height here in px, em or % if you want to budge your layout now the navigation is missing.
 						meanShowChildren: true, // true to show children in the menu, false to hide them
 						meanExpandableChildren: true, // true to allow expand/collapse children
--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,3 @@
 jQuery(document).ready(function () {
-	jQuery('div nav').meanmenu();
+	jQuery('header nav').meanmenu();
 });
